56dca5722f8290763194a4bbdf3c0663932f976f
shrutikumar2016 <73030171+shrutikumar2016@users.noreply.github.com>
2021-02-23 12:06:32 -0600

    changes to make edit profile work

---
+++ fuelprediction/accounts/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import UserProfile,UserFuelForm
+# Register your models here.
+
+admin.site.register(UserProfile)
+# admin.site.register(UserFuelForm)
--- website/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, UserFuelForm
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.admin import User
-
-# Register your models here.
-admin.site.register(UserProfile)
-admin.site.register(UserFuelForm)

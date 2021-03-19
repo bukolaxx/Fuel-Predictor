94c3aaa4e9dff4f97d87740cdc344ca2bcce96f0
shrutikumar2016 <73030171+shrutikumar2016@users.noreply.github.com>
2021-03-18 22:51:11 -0500

    test cases for profile

--- fuelprediction/accounts/tests.py
+++ fuelprediction/accounts/tests.py
@@ -86,3 +86,98 @@ class Test_register(TestCase):
 
         form = RegistrationForm()
         self.assertFalse(form.is_valid())
+        
+class Test_Profile(TestCase):
+
+    def test_statuscode(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_template(self):
+        response = self.client.get('/profile/')
+        self.assertTemplateUsed(response, 'accounts/profile.html')
+
+
+class Test_logout(TestCase):
+
+    def test_statuscode(self):
+        response = self.client.get('/logout/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_template(self):
+        response = self.client.get('/logout/')
+        self.assertRedirects(response, '/login/')
+
+class Test_editprofile(TestCase):
+
+    def setUp(self):
+        # Create one user profile
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')   
+             
+        test_user1.userprofile.Full_Name='testuser1',
+        test_user1.userprofile.Address1 ='testaddress', 
+        test_user1.userprofile.City = 'testCity',
+        test_user1.userprofile.State = 'testState', 
+        test_user1.userprofile.Zipcode = 'Testcode'
+    
+        test_user1.save()
+   
+    def test_get(self):
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get('/edit_profile/')
+        self.assertEqual(response.status_code, 200)
+        #self.assertEqual(response.context['form'], AuthenticationForm())
+        self.assertTemplateUsed(response, 'accounts/edit_profile.html')
+
+    def test_post(self):
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.post('/edit_profile/', {
+            'Full_Name': 'test', 
+            'Address1': 'test', 
+            'City':'test', 
+            'State': 'AR',
+            'Zipcode':'test' })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/profile/')
+        var = User.objects.get(username='testuser1')
+        self.assertEqual(var.userprofile.Full_Name, 'test')
+        self.assertEqual(var.userprofile.Address1, 'test')
+        self.assertEqual(var.userprofile.City, 'test')
+        self.assertEqual(var.userprofile.State, 'AR')
+        self.assertEqual(var.userprofile.Zipcode, 'test')
+    
+class Test_fqf(TestCase):
+    def setUp(self):
+        # Create one user profile
+        test_user1 = User.objects.create_user(username='testuser1', password='1X<ISRUkw+tuK')  
+        self.user = test_user1
+        test_user1.save()
+
+               
+    def test_get(self):
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        response = self.client.get('/fuelform/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/fuelform.html')
+
+    def test_post(self):
+
+        self.client.login(username='testuser1', password='1X<ISRUkw+tuK')
+        print("client logged in")
+        #var = User.objects.get(username='testuser1')
+        #var = "test address"
+        response = self.client.post('/fuelform/', {
+            'user' : self.user.id,
+            'gallsRequested' : 2,
+            'deliveryDate' :'2020-05-03',
+            'suggPrice' : 2,
+            'deliveryAddress' : "test address" ,
+            'total' : 3.00})
+        self.assertEqual(response.status_code, 302)     
+        self.assertRedirects(response, '/profile/')
+        var = UserFuelForm.objects.get(user =self.user)
+        self.assertEqual(var.gallsRequested, 2)
+        self.assertEqual(var.deliveryDate, datetime.date(2020, 5, 3))
+        self.assertEqual(var.suggPrice, 2)
+        self.assertEqual(var.deliveryAddress, 'test address')
+        self.assertEqual(var.total, 3.00)

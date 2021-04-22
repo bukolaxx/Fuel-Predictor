cda30cdb7a208daf56f9b2649369d0ceb9cb4765
shrutikumar2016 <73030171+shrutikumar2016@users.noreply.github.com>
2021-04-22 12:00:33 -0500

    calling pricing module

--- fuelprediction/accounts/forms.py
+++ fuelprediction/accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
-from .models import UserProfile, UserFuelForm
+from .models import UserProfile, UserFuelForm, PricingModule
 from decimal import Decimal
 
 
